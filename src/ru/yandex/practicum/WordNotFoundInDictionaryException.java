ba4b652ff8d2ea2518659faac032fd6052821a33
User Androsenko <urij@MacBook-Air-Urij.local>
2026-04-02 23:34:13 +0200

    1

--- src/ru/yandex/practicum/WordNotFoundInDictionaryException.java
+++ src/ru/yandex/practicum/WordNotFoundInDictionaryException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum;
 
-public class WordNotFoundInDictionaryException extends Exception{
-    public WordNotFoundInDictionaryException(String message){
+public class WordNotFoundInDictionaryException extends Exception {
+    public WordNotFoundInDictionaryException(String message) {
         super(message);
     }
 }
